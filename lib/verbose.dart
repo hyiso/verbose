376fffb9af27a80c9fb2aa2382ba69fbd2e85c36
hyiso <mylaoda@gmail.com>
2023-09-15 22:10:08 +0800

    Support empty `namespace` and configurable `separator`

--- lib/verbose.dart
+++ lib/verbose.dart
@@ -1,3 +1,36 @@
 library verbose;
 
-export 'src/verbose_base.dart';
+import 'dart:io';
+
+///
+/// Verbose class,
+///
+class Verbose {
+  /// namesapce for logging message
+  final String namespace;
+
+  /// IOSink for this verbose, default to [stdout]
+  final IOSink sink;
+
+  /// Separator used between [namespace] and [message], default to ' - '
+  final String separator;
+
+  Verbose(
+    this.namespace, {
+    this.separator = ' - ',
+    IOSink? sink,
+  }) : sink = sink ?? stdout;
+
+  /// Make instance of Verbose callable as a Function
+  String? call(String message) {
+    if (enabled) {
+      message = [if (namespace.isNotEmpty) namespace, message].join(separator);
+      sink.writeln(message);
+      return message;
+    }
+    return null;
+  }
+
+  /// Whether verbose is enabled.
+  static bool get enabled => Platform.environment['VERBOSE'] == 'true';
+}
